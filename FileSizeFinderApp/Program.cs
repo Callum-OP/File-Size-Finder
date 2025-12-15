8e4d5d72c636c57e567a7b1b74f5b9a72d4713ca
Callum-OP <callumoprey@gmail.com>
2025-12-15 06:16:17 +0000

    Created basic C# .Net project

---
+++ FileSizeFinderApp/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- High File Size Finder ---");
+        Console.Write("Enter the path of the folder you want to scan (e.g., C:\\Users\\YourName): ");
+        string path = Console.ReadLine();
+
+        Console.WriteLine($"Path is: {path}");
+    }
+}
